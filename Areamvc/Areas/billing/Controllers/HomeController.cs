4659512ac89dcc6f18dfc7abd9ddbec06d8d4947
Zaibi <zaibi@gmail.com>
2020-07-11 13:21:03 +0500

    Areas In MVC 5

---
+++ Areamvc/Areas/billing/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Areamvc.Areas.billing.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: billing/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areamvc/Areas/billing/billingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Areamvc.Areas.billing
+{
+    public class billingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "billing";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "billing_default",
+                "billing/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
